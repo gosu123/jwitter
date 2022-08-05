19d9ec8e82589349a0ed3ffdb5460a19d5cbd2c0
JinJeon <junj4620@naver.com>
2022-08-05 14:54:34 +0900

    Create Account / Log in Firebase

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onChange = (event) => {
         const {target : {name, value}} = event;
         if (name === "Email") setEmail(value);
@@ -21,20 +22,23 @@ const Auth = () => {
                 // log in
                 const data = await authService.signInWithEmailAndPassword(email, password);
                 console.log(data);
-            }    
-            
+            }            
         } catch (error) {
-            console.log(error);
-        }
-        
+            console.log(error.message);
+        }  
     };
+
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+
     return(
         <div>
             <form onSubmit={onSubmit}>
                 <input name="Email" type="email" placeholder="Email" value={email} onChange={onChange} required/>
                 <input name="Password" type="password" placeholder="Password" value={password} onChange={onChange} required/>
                 <input type="submit" value={newAccount ? "Create Account" : "Log In"}/>
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with Github</button>
